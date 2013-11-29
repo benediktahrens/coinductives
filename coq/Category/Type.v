15930af964b524854e980f511ddfa66d53763056
Régis SPADOTTI <regis.spadotti@gmail.com>
2013-11-29 17:30:49 +0100

    Category of Coq's type

---
+++ coq/Category/Type.v
@@ -0,0 +1,36 @@
+(*----------------------------------------------------------------------------*)
+(*    Category of types                                                       *)
+(*----------------------------------------------------------------------------*)
+
+Require Import Theory.Category.
+
+(*
+ * RawCategory
+ *)
+Definition Type_RawCategory : RawCategory :=
+  {| Obj     := Type
+   ; Hom     := λ A B ∙ A → B
+   ; id      := λ A x ∙ x
+   ; compose := λ A B C f g x ∙ f (g x)
+   ; Hom_eq  := λ A B f₁ f₂ ∙ ∀ x, f₁ x = f₂ x |}.
+
+(*
+ * IsCategory
+ *)
+Definition Type_IsCategory : IsCategory Type_RawCategory.
+Proof. constructor.
+  + (* Hom_eq_Equivalence *)
+    intros A B; constructor; hnf; simpl; [ reflexivity | now symmetry | etransitivity ; eauto].
+  + (* left_id *)
+    now simpl.
+  + (* right_id *)
+    now simpl.
+  + (* assoc *)
+    now simpl.
+  + (* compose_cong *)
+    intros A B C f₁ f₂ eq_f₁f₂ g₁ g₂ eq_g₁g₂ x; simpl in *.
+    now rewrite eq_g₁g₂, eq_f₁f₂.
+Qed.
+
+Definition 𝑻𝒚𝒑𝒆 : Category := {| rawCategory := Type_RawCategory
+                              ; isCategory   := Type_IsCategory |}.
