f5cd9b663030724bfc105e1258123296a93dd2c4
rs- <sev3n.rs@gmail.com>
2013-12-10 12:44:14 +0100

    Strong Monoidal definition

---
+++ coq/Theory/StrongMonoidal.v
@@ -0,0 +1,18 @@
+Require Import Theory.Category.
+Require Import Theory.Morphism.
+Require Import Theory.Functor.
+Require Import Theory.Product.
+
+Generalizable All Variables.
+Set Implicit Arguments.
+Unset Strict Implicit.
+
+(*------------------------------------------------------------------------------
+  -- ＳＴＲＯＮＧ  ＭＯＮＯＩＤＡＬ  ＦＵＮＣＴＯＲ  ＤＥＦＩＮＩＴＩＯＮ
+  ----------------------------------------------------------------------------*)
+
+(** ¡¡ Very 'direct' definition !! **)
+Class StrongMonoidal `{BinaryProduct 𝒞} `{BinaryProduct 𝒟} (F : Functor 𝒞 𝒟) :=
+  strong_monoidal : ∀ {A B : 𝒞}, F (A × B) ≅ F A × F B.
+
+Notation "F -×" := (strong_monoidal (F0 := F)) (at level 0, only parsing).
